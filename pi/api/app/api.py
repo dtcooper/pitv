8884eca847f6418399f197a00a58a34fb8ca1c7c
David Cooper <david@dtcooper.com>
2022-06-19 23:37:58 -0400

    Add sse

--- pi/api/app/api.py
+++ pi/api/app/api.py
@@ -1,13 +1,38 @@
+import asyncio
 from aiohttp import web
+from aiohttp.web import Response
+from aiohttp_sse import sse_response
+from datetime import datetime
 
-async def handle(request):
-    name = request.match_info.get('name', "Anonymous")
-    text = "Hello, " + name
-    return web.Response(text=text)
+
+async def hello(request):
+    async with sse_response(request) as resp:
+        while True:
+            data = 'Server Time : {}'.format(datetime.now())
+            print(data)
+            await resp.send(data)
+            await asyncio.sleep(1)
+
+
+async def index(request):
+    d = """
+        <html>
+        <body>
+            <script>
+                var evtSource = new EventSource("/hello");
+                evtSource.onmessage = function(e) {
+                    document.getElementById('response').innerText = e.data
+                }
+            </script>
+            <h1>Response from server:</h1>
+            <div id="response"></div>
+        </body>
+    </html>
+    """
+    return Response(text=d, content_type='text/html')
+
 
 app = web.Application()
-app.add_routes([web.get('/', handle),
-                web.get('/{name}', handle)])
-
-if __name__ == '__main__':
-    web.run_app(app)
+app.router.add_route('GET', '/hello', hello)
+app.router.add_route('GET', '/', index)
+web.run_app(app, port=8080)
