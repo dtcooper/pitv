fbc4055e835dd8c7bcd9d2227c022b6ed773e007
David Cooper <david@dtcooper.com>
2023-12-13 15:51:04 -0500

    Testing builder action

---
+++ build/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+tar --skip-old-files --absolute-names -xf /mounted-github-repo/omxplayer-dist.tgz
+
+# TODO: Only do on arm64
+dpkg --add-architecture armhf
+
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
+    fonts-freefont-ttf \
+    libasound2:armhf \
+    libavcodec58:armhf \
+    libavformat58:armhf \
+    libc6:armhf \
+    libdbus-1-3:armhf \
+    libdvdread8:armhf \
+    libraspberrypi0:armhf
+
+# TODO: Only do on arm64
+for lib in bcm_host vchiq_arm vcos; do
+    sudo ln -vs "/usr/lib/arm-linux-gnueabihf/lib${lib}.so.0" "/usr/lib/arm-linux-gnueabihf/lib${lib}.so"
+done
