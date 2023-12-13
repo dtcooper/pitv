fee9883763d4dc7e492b2327b43ccc339e7752d4
David Cooper <david@dtcooper.com>
2023-12-13 16:48:10 -0500

    Add Python 3.12 to image

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -2,12 +2,15 @@
 
 set -e
 
+PYTHON_VERSION=3.12.1
+
 tar --skip-old-files --absolute-names -xf /mounted-github-repo/build/omxplayer-dist.tgz
 
 # TODO: Only do on arm64
 dpkg --add-architecture armhf
 
 apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get upgrade -y
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
     fonts-freefont-ttf \
     libasound2:armhf \
@@ -16,9 +19,32 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends \
     libc6:armhf \
     libdbus-1-3:armhf \
     libdvdread8:armhf \
-    libraspberrypi0:armhf
+    libraspberrypi0:armhf \
+    build-essential \
+    libbz2-dev \
+    libc6-dev \
+    libffi-dev \
+    libgdbm-dev \
+    liblzma-dev \
+    libncurses5-dev \
+    libncursesw5-dev \
+    libreadline-dev \
+    libsqlite3-dev \
+    libssl-dev \
+    openssl \
+    zlib1g-dev
 
 # TODO: Only do on arm64
 for lib in bcm_host vchiq_arm vcos; do
     sudo ln -vs "/usr/lib/arm-linux-gnueabihf/lib${lib}.so.0" "/usr/lib/arm-linux-gnueabihf/lib${lib}.so"
 done
+
+# Install newest version of Python
+mkdir -p /tmp/python
+cd /tmp/python
+curl -sL "https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tar.xz" | tar xJ --strip-components=1
+./configure --enable-optimizations
+make -j "$(nproc)"
+make altinstall
+cd /tmp
+rm -rf /tmp/python
