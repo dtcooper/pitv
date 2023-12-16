aaf8c53a0f5f243afc6a7859ee4992060b24d3b8
David Cooper <david@dtcooper.com>
2023-12-16 15:02:45 -0500

    Update install script

--- install.sh
+++ install.sh
@@ -2,25 +2,28 @@
 
 set -e
 
+CONTAINER_NAME=ghcr.io/dtcooper/pitv-player
+
 CMD="$0"
 usage() {
     cat <<EOF
-Usage: ${CMD} [--local-image <path>] [--yes] [--dry-run]
+Usage: ${CMD} [-l <path>] [--force-docker] [-y] [-n] [-h]
 
 Script to install PiTV player on your Raspberry Pi
 
 Arguments:
-  -l <path>, --local-image <path>  Usage local image at <path>, don't pull one
-  -y, --yes                        Answer yes to all prompts
-  -n, --dry-run                    Dry run, don't actually install
-  -v, --verbose                    Run verbosely
-  -h, --help                       Show this help screen
+  -l <path>, --local-image <path>   Usage local image at <path>, don't pull one
+  --force-docker                    Force Docker installation, even it exists
+  -y, --yes                         Answer yes to all prompts
+  -n, --dry-run                     Dry run, don't actually install
+  -v, --verbose                     Run verbosely
+  -h, --help                        Show this help screen
 EOF
     exit 0
 }
 
-
 LOCAL_IMAGE=
+FORCE_DOCKER=
 DRY_RUN=
 VERBOSE=
 YES=
@@ -29,6 +32,9 @@ while [ $# -gt 0 ]; do
         --local-image|-l)
             LOCAL_IMAGE="$2"
             shift
+            ;;
+        --force-docker)
+            FORCE_DOCKER=1
             ;;
         --yes|-y)
             YES=1
@@ -63,12 +69,44 @@ sh_exec() {
     fi
 }
 
+test_cmd() {
+    which "$1" > /dev/null
+}
+
+install_docker() {
+    if [ -z "$FORCE_DOCKER" ] && test_cmd docker; then
+        echo 'Docker appears to already be installed. Skipping Docker installation!'
+    else
+        if [ "$DRY_RUN" ]; then
+            DOCKER_INSTALL_SCRIPT=/tmp/install-docker.sh
+        else
+            DOCKER_INSTALL_SCRIPT="$(mktemp -t install-docker-XXXXXXXX.sh)"
+        fi
+        sh_exec curl -fsSLo "${DOCKER_INSTALL_SCRIPT}" https://get.docker.io/
+        sh_exec sh "${DOCKER_INSTALL_SCRIPT}"
+        sh_exec rm "${DOCKER_INSTALL_SCRIPT}"
+    fi
+}
+
+load_container() {
+    if [ "$LOCAL_IMAGE" ]; then
+        sh_exec docker load -i "${LOCAL_IMAGE}"
+    else
+        sh_exec docker pull "${CONTAINER_NAME}"
+    fi
+}
+
+cd "$(dirname "${CMD}")"
+
 if [ "$(id -u)" -ne 0 ]; then
     echo 'You must run this script as root! (Try it with sudo)'
     exit 1
 fi
 
-echo "LOCAL_IMAGE=${LOCAL_IMAGE}"
-echo "DRY_RUN=${DRY_RUN}"
-echo "VERBOSE=${VERBOSE}"
-echo "YES=${YES}"
+if [ ! -e docker-compose.yml ]; then
+    echo "Could not find docker-compose.yml. Are you sure you're in the PiTV repo?"
+    exit 1
+fi
+
+install_docker
+load_container
